71be30961cae9f24eada86d97724a62784b99687
nireo <eemil.lehtikangas1@gmail.com>
2020-09-16 11:16:51 +0300

    added my dwm bar scripts

---
+++ dwm_bar.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+# A modular status bar for dwm
+# Joe Standring <git@joestandring.com>
+# GNU GPLv3
+
+# Dependencies: xorg-xsetroot
+
+# Import functions with "$include /route/to/module"
+# It is recommended that you place functions in the subdirectory ./bar-functions and use: . "$DIR/bar-functions/dwm_example.sh"
+
+# Store the directory the script is running from
+LOC=$(readlink -f "$0")
+DIR=$(dirname "$LOC")
+
+# Change the appearance of the module identifier. if this is set to "unicode", then symbols will be used as identifiers instead of text. E.g. [📪 0] instead of [MAIL 0].
+# Requires a font with adequate unicode character support
+export IDENTIFIER=""
+
+# Change the charachter(s) used to seperate modules. If two are used, they will be placed at the start and end.
+export SEP1="["
+export SEP2="]"
+
+# Import the modules
+. "$DIR/dwm_date.sh"
+. "$DIR/dwm_resources.sh"
+. "$DIR/dwm_keyboard.sh"
+. "$DIR/dwm_spotify.sh"
+
+# Update dwm status bar every second
+while true
+do
+
+    # Append results of each func one by one to a string
+    dispstr=""
+    dispstr="$dispstr$(dwm_spotify)"
+    dispstr="$dispstr$(dwm_keyboard)"
+    dispstr="$dispstr$(dwm_resources)"
+    dispstr="$dispstr$(dwm_date)"
+
+    xsetroot -name "$dispstr"
+    sleep 1
+
+done
